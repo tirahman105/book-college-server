4adfb945cc7ff5473ba9e355c631fa5b9cbd5f71
tirahman105 <tirahman105@gmail.com>
2023-07-24 23:24:01 +0600

    unecessary comments dlt

--- index.js
+++ index.js
@@ -1,100 +1,3 @@
-// const express = require('express')
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const cors = require('cors')
-// const app = express()
-// require('dotenv').config()
-// const port = process.env.PORT || 5000
-
-
-
-
-// // middleware
-// app.use(cors());
-// app.use(express.json())
-
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kx4dtgt.mongodb.net/?retryWrites=true&w=majority`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-//     const usersCollection = client.db("collegeDB").collection("users");
-//     const collegeCollection = client.db('collegeDB').collection('collegeCollection');
-
-
-//     app.post('/users', async (req, res) => {
-//         const user = req.body;
-//         console.log(user);
-//         const query = { email: user.email }
-//         const existingUser = await usersCollection.findOne(query);
-//           console.log('existing user' , existingUser)
-//         if (existingUser) {
-//           return res.send({ message: 'user already exists' })
-//         }
-  
-
-//         const result = await usersCollection.insertOne(user);
-//         res.send(result);
-//       })
-
-//       app.get('/users/details/:id', async(req, res) => {
-//         const id = req.params.id;
-//         const query = {_id: new ObjectId (id)}
-//         const result = await usersCollection.findOne(query);
-//         res.send(result);
-//       })
-
-//       app.get('/colleges', async(req, res) => {
-//         const cursor = collegeCollection.find();
-//         const result = await cursor.toArray();
-//         res.send(result)
-//       })
-  
-//       app.get('/colleges/details/:id', async(req, res) => {
-//         const id = req.params.id;
-//         const query = {_id: new ObjectId (id)}
-//         const result = await collegeCollection.findOne(query);
-//         res.send(result);
-//       })
-  
-    
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-
-
-// app.get('/', (req, res) => {
-//   res.send(' server is running!')
-// })
-
-
-// app.listen(port, () => {
-//   console.log(`app listening on port ${port}`)
-// })
-
-
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
@@ -120,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("collegeDB").collection("users");
     const collegeCollection = client.db('collegeDB').collection('collegeCollection');
